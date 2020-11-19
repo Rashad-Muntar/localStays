c4a31be7013ab3a1f1048105d65f2000156a2e5d
unknown <muntartoure@gmail.com>
2020-11-19 14:16:53 +0000

    add cookie banner

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,15 @@
-var coookies = document.getElementById('cookies');
-var btn = document.getElementById('cookies-btns');
 
-btn.onclick = function() {
-      cookies.style.display = 'none';
-  };
+const cookies = document.querySelector('.cookies');
+const btn = document.querySelector('.accept');
+
+
+btn.addEventListener("click", () =>{
+  cookies.classList.remove("active")
+  localStorage.setItem("cookieBannerDisplay", "true")
+})
+
+setTimeout(()=>{
+  if(!localStorage.getItem("cookieBannerDisplay")){
+    cookies.classList.add("active")
+  }
+}, 2000)
